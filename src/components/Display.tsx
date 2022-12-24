e141ef4797e1dd7a2e35c0a52970b8bfba2a240b
Joseph Wang <jgwang@ad.unc.edu>
2022-12-24 15:10:11 -0500

    trying to display a pdf

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Document, Page } from 'react-pdf';
 
 export class Display extends React.Component {
     render() {
         return (
-            <div>
-                <iframe src="../files/resume.pdf" height="200" width="300"></iframe>
-                <h1>working</h1>
-            </div>
+            <Document file="../files/resume.pdf">
+                <Page pageNumber = {1} />
+            </Document>
         )
     }
 }
+
+{/* <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
+<p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+</object> */}
