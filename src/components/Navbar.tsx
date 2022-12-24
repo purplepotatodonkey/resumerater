e320dfcf8217dafca095228e0dfaaa561e718323
Joseph Wang <jgwang@ad.unc.edu>
2022-12-24 14:00:48 -0500

    import display

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import Navbar from './components/Navbar';
+import Display from './components/Display';
 import './App.css';
 
 export class Navbar extends React.Component {
