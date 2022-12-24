080a7fbf7e9dda31ec1f0e43053d4b093fb2fd0d
matt <mat@unc.edu>
2022-12-24 14:00:34 -0500

    test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../App.css';
 
 export class Navbar extends React.Component {
   render(): React.ReactNode {
