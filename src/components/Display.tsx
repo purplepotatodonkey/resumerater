31055de6a7a75ab4fa344d1e9efe63dedc24d063
Joseph Wang <jgwang@ad.unc.edu>
2022-12-24 15:35:46 -0500

    new approach

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf';
 
 export class Display extends React.Component {
-    render() {
-        return (
-            <Document file="../files/resume.pdf">
-                <Page pageNumber = {1} />
-            </Document>
-        )
-    }
+	state = { numPages: null, pageNumber: 1 };
+
+	onDocumentLoadSuccess = ({ numPages }) => {
+		this.setState({ numPages });
+	};
+
+	goToPrevPage = () =>
+		this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
+	goToNextPage = () =>
+		this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
+
+	render() {
+		const { pageNumber, numPages } = this.state;
+
+		return (
+			<div>
+				<nav>
+					<button onClick={this.goToPrevPage}>Prev</button>
+					<button onClick={this.goToNextPage}>Next</button>
+				</nav>
+
+				<div style={{ width: 600 }}>
+					<Document
+						file="../files/resume.pdf"
+						onLoadSuccess={this.onDocumentLoadSuccess}
+					>
+						<Page pageNumber={pageNumber} width={600} />
+					</Document>
+				</div>
+
+				<p>
+					Page {pageNumber} of {numPages}
+				</p>
+			</div>
+		);
+	}
+
 }
 
 {/* <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
