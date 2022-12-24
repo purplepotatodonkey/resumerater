b82fe13d513a155a55775123645543d72452e3c0
matt <mat@unc.edu>
2022-12-24 14:39:10 -0500

    test

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 5000
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const fs = requre('fs');
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+app.post('/upload', upload.single('pdf'), (req, res) => {
+    fs.writeFile(req.file.path, req.file.buffer, err => {
+      if (err) {
+        // Handle the error
+      } else {
+        res.send('PDF uploaded successfully');
+      }
+    });
+  });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,88 +1,18 @@
-import * as React from 'react';
-// import { button } from 'react'
+import React from 'react';
+import '../App.css';
 
-const AcceptedFileType = {
-  Text: '.txt',
-  Gif: '.gif',
-  Jpeg: '.jpg',
-  Png: '.png',
-  Doc: '.doc',
-  Pdf: '.pdf',
-  AllImages: 'image/*',
-  AllVideos: 'video/*',
-  AllAudios: 'audio/*',
-};
-
-export default function Upload({ fileType }) {
-  const fileRef = React.useRef();
-  const acceptedFormats =
-    typeof fileType === 'string'
-      ? fileType
-      : Array.isArray(fileType)
-      ? fileType?.join(',')
-      : AcceptedFileType.Text;
-
-  const [selectedFiles, setSelectedFiles] = React.useState();
-
-  const handleFileSelect = (event) => {
-    setSelectedFiles(event?.target?.files?.[0]);
-  };
-
-  const onUpload = () => {
-    console.log(selectedFiles);
-  };
-
-  const onClear = () => {
-    setSelectedFiles(undefined);
-  };
-
-  const onUpdate = (event) => {
-    if (event.target.textContent.trim().toLowerCase() === 'change') {
-      onClear();
-      fileRef.current.click();
-      return;
-    }
-    if (event.target.textContent.trim().toLowerCase() === 'clear') {
-      onClear();
-      return;
-    }
-  };
-
-  return (
-    <>
-      <input
-        ref={fileRef}
-        hidden
-        type="file"
-        accept={acceptedFormats}
-        onChange={handleFileSelect}
-      />
-      {!selectedFiles?.name && (
-        <button
-          style={{ textTransform: 'none' }}
-          onClick={() => fileRef.current?.click()}
-        >
-          Choose file to upload
-        </button>
-      )}
-      {selectedFiles?.name && (
-        <button
-          style={{ textTransform: 'none' }}
-          onClick={onUpdate}
-        >
-          <span style={{ float: 'left' }}> {selectedFiles?.name}</span>
-          <span style={{ padding: '10px' }}> Change</span>
-          <span>Clear</span>
-        </button>
-      )}
-      <button
-        color="primary"
-        disabled={!selectedFiles}
-        style={{ textTransform: 'none' }}
-        onClick={onUpload}
-      >
-        Upload
-      </button>
-    </>
-  );
+export class App extends React.Component {
+  render(): React.ReactNode {
+    return (
+      <div className="App">
+        <div>NAVBAR COMPONENT 😄</div>
+        <form action="/upload" method="post" encType="multipart/form-data">
+          <input type="file" name="pdf" accept="application/pdf"></input>
+          <button type="submit">Upload PDF</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default App;
