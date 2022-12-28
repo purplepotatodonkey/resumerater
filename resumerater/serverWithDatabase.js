f2526f61e677f63208a5c396f56bd121c1117f19
matt <mat@unc.edu>
2022-12-28 18:36:59 -0500

    worked withh server with database js

--- resumerater/serverWithDatabase.js
+++ resumerater/serverWithDatabase.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
       console.log(output)
       for(i=0;i<output;i++) {
         console.log('inserting entry ' + i + ' from files dir');
-        let incmd = "files=(/root/resumerater/resumerater/uploads/*);echo ${files[" + i + "]} | cut -d'/' -f 6)"
+        let incmd = "files=(/root/resumerater/resumerater/uploads/*);echo ${files[" + i + "]} | cut -d'/' -f 6"
         exec(incmd, (err, stdout, stderr) => {
           if (err !== null) {
             console.log('exec error: ' + err);
