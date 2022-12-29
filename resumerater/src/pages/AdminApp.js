0842ee005bf0959f633a120ecb207d7a85d780e0
matt <mat@unc.edu>
2022-12-29 00:40:46 -0500

    changes to overall project

--- resumerater/src/pages/AdminApp.js
+++ resumerater/src/pages/AdminApp.js
@@ -102,23 +102,6 @@ function AdminApp() {
   //   });
   // };
 
-  const getRandomPDF = async(e) => {
-      console.log("Fetching 2 random pdfs from server...")
-      const response = await fetch('http://139.177.207.245:5000/random', {
-        method: 'GET'
-      })
-      const data = await response.text();
-      console.log(data)
-      const response2 = await fetch('http://139.177.207.245:5000/random', {
-        method: 'GET'
-      })
-      const data2 = await response2.text();
-      console.log(data2)
-      setPdfstr1("http://139.177.207.245:5000/uploads/" + data);
-      setPdfstr2("http://139.177.207.245:5000/uploads/" + data2);
-      setResAPI("Successfully retrieved Random PDF")
-  }
-
   const handleFileChangeData = (data) => {
     if (data) {
       console.log("Handling new data")
@@ -154,8 +137,6 @@ function AdminApp() {
     {/* </form> */}
 
       <br></br>
-      <button onClick={getRandomPDF}>Get 2 Random PDFs From Database</button>
-      <br></br>
       <button onClick={getAllResumesFromDB}>Console Log All Resumes from DB</button>
       <br></br>
       <button onClick={resetEntireDB}>Reset Entire DB</button>
@@ -166,26 +147,6 @@ function AdminApp() {
       <br></br>
       <div>{resAPI}</div>
       <br></br>
-      <div style={{position:"fixed",height:"100%",left:"5%",display:'inline', width:"40%",border:"5px solid gray",fontSize:"1px"}}>
-        {(pageNumber1<numPages1)&&<button onClick={(e) => setPageNumber1(pageNumber1+1)}>+</button>}
-        {(pageNumber1>1)&&<button onClick={(e) => setPageNumber1(pageNumber1-1)}>-</button>}
-        <Document file={pdfstr1} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber1}/>
-        </Document>
-        <p>
-          Page {pageNumber1} of {numPages1}
-        </p>
-      </div>
-      <div style={{position:"fixed",height:"100%",right:"5%", display:'inline', width:"40%",border:"5px solid gray",fontSize:"1px"}}>
-        {(pageNumber2<numPages2)&&<button onClick={(e) => setPageNumber2(pageNumber2+1)}>+</button>}
-        {(pageNumber2>1)&&<button onClick={(e) => setPageNumber2(pageNumber2-1)}>-</button>}
-        <Document file={pdfstr2} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber2}/>
-        </Document>
-        <p>
-          Page {pageNumber2} of {numPages2}
-        </p>
-      </div>
     </div>
   );
 }
